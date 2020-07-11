4a75254cdfaf85fe440ecb5f48c436f107cd6674
Het Patel <hetpatel91314@gmail.com>
2020-07-11 19:21:59 -0400

    Progress-1

--- visualize.java
+++ visualize.java
@@ -16,6 +16,10 @@ import javax.swing.SwingUtilities;
 
 public class visualize extends JPanel {
     List<Double> scores =  new ArrayList();
+    int padding=20;
+    int lablepadding=12;
+    int numberYDivisions=6;
+
 
     public visualize(List<Double> scores){
         this.scores=scores;
@@ -45,9 +49,30 @@ public class visualize extends JPanel {
     public void paintComponent(Graphics g){
         super.paintComponent(g);
 
-        // Graphics2D g2=(Graphics2D) g;
-        // g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-    }
+        Graphics2D g2=(Graphics2D) g;
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+
+        double scaleX=((double)getWidth()-(3* padding)-labelpadding)/(scores.size()-1);
+        double scaleY=((double)getWidth()-(3* padding)-labelpadding)/(getMaxScore()-getMinScores());
+
+        List<Point> graphPoints = new ArrayList<>();
+        for(int i=0;i<scores.size();i++){
+            int x1=(int)(i*scaleX+padding+labelpadding);
+            int y1=(int)((getMaxScore()-scores.get(i)*scaleY)+padding);
+            graphPoints.add(new Point(x1,y1));
+        }
+
+        g2.setColor(COLOR.WHITE);
+        g2.fillRect(padding+labelpadding,padding,getWidth()-(2*padding)-labelpadding,getHeight()-2*padding-labelpadding);
+        g2.setColor(Color.BLUE);
+
+        for(int i=0;i<numberYDivisions+1;i++){
+            int x0=padding+labelpadding;
+                int x1=pointWidth+labelpadding+padding;
+                int y0=getHeight()-((i*(getHeight()-padding*2-labelpadding))/numberYDivison+padding+labelpadding)
+        }
+
+    } 
     public static void main(String args[]){
         SwingUtilities.invokeLater(new Runnable(){
             public void run(){
