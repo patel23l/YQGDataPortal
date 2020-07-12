ef0560ee61540dfe072f50ea27258802fc284b09
nandinipatel <patelnandini26@gmail.com>
2020-07-12 13:15:49 -0400

    added vote values on y-axis

--- visualize.java
+++ visualize.java
@@ -17,40 +17,47 @@ import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
 
 public class visualize extends JPanel {
-    List<Double> scores =  new ArrayList();
+    //List<Double> scores =  new ArrayList();
+    //List<Double> ward = new ArrayList<>();
+    //List<String> names = new ArrayList<>();
+    List<Double> votes = new ArrayList<>();
+    
     int padding=20;
     int labelpadding=12;
     int numberYDivisions=6;
     int  pointWidth = 10 ;  
 
-    private Color gridColor=new Color(200,200,200,200);
+    private Color gridColor = new Color(200,200,200,200);
 
     private double getMinScores() { 
         double minScore = Double.MAX_VALUE;
-        for (Double score : scores) {
-            minScore = Math.min(minScore, score);
+        for (Double vote : votes) {
+            minScore = Math.min(minScore, vote);
         } 
         return minScore;
         }
 
         private double getMaxScore() {
         double maxScore = Double.MIN_VALUE;
-        for (Double score : scores) {
-            maxScore = Math.max(maxScore, score);
+        for (Double vote : votes) {
+            maxScore = Math.max(maxScore, vote);
         }
         return maxScore;
     }
 
-    public visualize(List<Double> scores){
-        this.scores=scores;
+    public visualize(List<Double> votes){
+        this.votes = votes;
     }
 
     public static void createGui(){
-        //===========================================
-        List<String> ward = new ArrayList<>();
+        Random random = new Random();
+        //List<Double> scores =  new ArrayList();
+        //==========================================
+        List<Double> ward = new ArrayList<>();
         List<String> names = new ArrayList<>();
-        List<String> votes = new ArrayList<>();
-        //===========================================
+        List<Double> votes = new ArrayList<>();
+        double temp;
+
         try {
             File myObj = new File("input.txt");
             Scanner Reader = new Scanner(myObj);
@@ -58,26 +65,27 @@ public class visualize extends JPanel {
             while (Reader.hasNextLine()) {
                 String data = Reader.nextLine();
                 String[] separation = data.split(",");
-                ward.add(separation[0]);
+                temp = Double.parseDouble(separation[0]);
+                ward.add(temp);
                 names.add(separation[1]);
-                votes.add(separation[2]);
+                temp = Double.parseDouble(separation[2]);
+                votes.add(temp);
             }
             Reader.close();
         }
         catch (Exception e) {
             System.out.println(e);
         }
-        //===========================================
-        Random random = new Random();
-    List<Double> scores =  new ArrayList();
-
+        //==========================================
         int maxDataPoints=20;
         int maxScore=8;
 
+        /*
         for(int i=0;i<maxDataPoints;i++){
             scores.add((double)random.nextDouble()*maxScore);
-        }
-        visualize mainPanel=new visualize(scores);
+        } */
+
+        visualize mainPanel = new visualize(votes);
         mainPanel.setPreferredSize(new Dimension(700,600));
 
         JFrame frame = new JFrame("A visualize dataset");
@@ -94,13 +102,13 @@ public class visualize extends JPanel {
         Graphics2D g2=(Graphics2D) g;
         g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 
-        double scaleX=((double)getWidth()-(3* padding)-labelpadding)/(scores.size()-1);
+        double scaleX=((double)getWidth()-(3* padding)-labelpadding)/(votes.size()-1);
         double scaleY=((double)getWidth()-(3* padding)-labelpadding)/(getMaxScore()-getMinScores());
 
         List<Point> graphPoints = new ArrayList<>();
-        for(int i=0;i<scores.size();i++){
+        for(int i=0;i<votes.size();i++){
             int x1=(int)(i*scaleX+padding+labelpadding);
-            int y1=(int)((getMaxScore()-scores.get(i)*scaleY)+padding);
+            int y1=(int)((getMaxScore()-votes.get(i)*scaleY)+padding);
             graphPoints.add(new Point(x1,y1));
         }
 
@@ -114,7 +122,7 @@ public class visualize extends JPanel {
             int y0=getHeight()-((i*(getHeight()-padding*2-labelpadding))/numberYDivisions+padding+labelpadding);
             
             int y1=y0;
-            if(scores.size()>0){
+            if(votes.size()>0){
                 g2.setColor(gridColor);
                 g2.drawLine(padding+labelpadding+1+pointWidth,y0,getWidth()-padding,y1);
                 g2.setColor(Color.BLACK);
